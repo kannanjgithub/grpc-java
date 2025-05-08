670dbb6d42f854e01e43fc0430a194d4b24b20c9
deadEternally <kannanjgithub@google.com>
2025-05-08 16:52:46 +0530

    Server interceptor creating context with a key, and with Otel tracing enabled. To try repro b/416307660.

--- examples/example-oauth/src/main/java/io/grpc/examples/oauth/AuthServer.java
+++ examples/example-oauth/src/main/java/io/grpc/examples/oauth/AuthServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The gRPC Authors
+ * Copyright 2024 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,49 +19,39 @@ package io.grpc.examples.oauth;
 import io.grpc.Grpc;
 import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.opentelemetry.GrpcOpenTelemetry;
 import io.grpc.stub.StreamObserver;
+import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 /**
- * Server that manages startup/shutdown of a {@code Greeter} server. This also uses a
- * {@link OAuth2ServerInterceptor} to intercept the OAuth2 token passed.
+ * gRPC server that manages startup/shutdown of a {@code Greeter} server and generates
+ * gRPC OpenTelemetry metrics data based on the configuration.
  */
 public class AuthServer {
-
   private static final Logger logger = Logger.getLogger(AuthServer.class.getName());
 
   private Server server;
-  private int port;
-
-  public AuthServer(int port) {
-    this.port = port;
-  }
-
-  private void start() throws IOException {
-    server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
-        .addService(new GreeterImpl())
-        .intercept(new OAuth2ServerInterceptor())
-        .build()
-        .start();
+  private void start(int port) throws IOException {
+    ServerBuilder<?> serverBuilder = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+            .addService(new GreeterImpl());
+    server = serverBuilder.intercept(new OAuth2ServerInterceptor())
+            .build()
+            .start();
     logger.info("Server started, listening on " + port);
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        AuthServer.this.stop();
-        System.err.println("*** server shut down");
-      }
-    });
   }
 
-  private void stop() {
+  private void stop() throws InterruptedException {
     if (server != null) {
-      server.shutdown();
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
     }
   }
 
@@ -78,19 +68,72 @@ public class AuthServer {
    * Main launches the server from the command line.
    */
   public static void main(String[] args) throws IOException, InterruptedException {
+    // The port on which the server should run.
+    int port = 50051;
+    // The port on which prometheus metrics are exposed.
+    int prometheusPort = 9464;
 
-    // The port on which the server should run
-    int port = 50051; // default
     if (args.length > 0) {
+      if ("--help".equals(args[0])) {
+        System.err.println("Usage: [port [prometheus_port]]");
+        System.err.println("");
+        System.err.println("  port  The port on which server will run. Defaults to " + port);
+        System.err.println("  prometheusPort  The port to expose prometheus metrics. Defaults to " + prometheusPort);
+        System.exit(1);
+      }
       port = Integer.parseInt(args[0]);
     }
+    if (args.length > 1) {
+      prometheusPort = Integer.parseInt(args[1]);
+    }
 
-    final AuthServer server = new AuthServer(port);
-    server.start();
+    // Adds a PrometheusHttpServer to convert OpenTelemetry metrics to Prometheus format and
+    // expose these via a HttpServer exporter to the SdkMeterProvider.
+    SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+            .registerMetricReader(
+                    PrometheusHttpServer.builder().setPort(prometheusPort).build())
+            .build();
+
+    // Initialize OpenTelemetry SDK with MeterProvider configured with Prometheus metrics exporter
+    OpenTelemetrySdk openTelemetrySdk =
+            OpenTelemetrySdk.builder().setMeterProvider(sdkMeterProvider).build();
+
+    // Initialize gRPC OpenTelemetry.
+    // Following client metrics are enabled by default :
+    //     1. grpc.server.call.started
+    //     2. grpc.server.call.sent_total_compressed_message_size
+    //     3. grpc.server.call.rcvd_total_compressed_message_size
+    //     4. grpc.server.call.duration
+    GrpcOpenTelemetry grpcOpenTelmetry = GrpcOpenTelemetry.newBuilder()
+            .sdk(openTelemetrySdk)
+            .build();
+    // Registers gRPC OpenTelemetry globally.
+    grpcOpenTelmetry.registerGlobal();
+
+    final AuthServer server = new AuthServer();
+    server.start(port);
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          server.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        // Shut down OpenTelemetry SDK.
+        openTelemetrySdk.close();
+
+        System.err.println("*** server shut down");
+      }
+    });
+
     server.blockUntilShutdown();
   }
 
   static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
       // get client id added to context by interceptor
